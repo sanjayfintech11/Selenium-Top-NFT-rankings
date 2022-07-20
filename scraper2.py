8b3feb57fa85ef84f1e2e3656dc15baa4c1b51c0
Sanjay Veeraragavulu <85775960+sanjayfintech11@users.noreply.github.com>
2022-07-20 09:25:08 +0000

    Added Flask

--- scraper2.py
+++ scraper2.py
@@ -1,1 +1,8 @@
-print('Hello WOrld')
+from flask import Flask
+app= Flask(__name__)
+@app.route("/")
+def hello_world():
+  return "Hello, Jovian"
+
+if __name__== "__main__":
+  app.run(host='0.0.0.0', port=80, debug=True)
